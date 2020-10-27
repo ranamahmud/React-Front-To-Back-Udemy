6309eb59f278c32c65fadfe423364151643e71b3
Md Rana Mahmud <ranamd0@gmail.com>
2020-10-27 13:25:52 +0600

    serach ,alert added

--- Section 2 Project 1 Start Github/github-finder/src/App.js
+++ Section 2 Project 1 Start Github/github-finder/src/App.js
@@ -4,29 +4,49 @@ import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import axios from 'axios'
 import Search from './components/Search';
+import Alert from './components/layout/Alert';
 class App extends React.Component {
   state = {
     users: [],
-    loading: false
-  }
-  async componentDidMount() {
-    console.log(process.env.REACT_APP_CLIENT_ID)
-    this.setState({ loading: true })
-    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_CLIENT_ID}&client_secret=${process.env.REACT_APP_CLIENT_SECRET}`)
-    this.setState({ users: res.data, loading: false })
-
+    loading: false,
+    alert: null
   }
 
+
+  // Search github users
+  searchUsers = async (text) => {
+    this.setState({ loading: true });
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_CLIENT_ID}&client_secret=${process.env.REACT_APP_CLIENT_SECRET}`)
+    this.setState({ users: res.data.items, loading: false })
+  }
+
+  // clear users from state
+
+  clearUsers = () => {
+    this.setState({ users: [], loading: false })
+  }
+
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } })
+    setTimeout(() => {
+      this.setState({ alert: null })
+    }, 5000)
+  }
   render() {
-
+    const { users, loading } = this.state;
     return (
       <div className="App">
         <Navbar
         />
-        <Search />
         <div className="container">
+          <Alert alert={this.state.alert} />
+          <Search searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
+          />
 
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Users loading={loading} users={users} />
 
         </div>
 
